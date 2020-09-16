727f8e215719803d4bf18a7d1ddfcc4abd1ec73a
rkemp <rkemp@herzog.com>
2020-09-16 16:32:49 -0500

    update kml parsing logic, update request headers to impersonate normal request from a browser, update timing logic to mitigate premature requests of non-existant kml files, update error handling

--- get-kml.py
+++ get-kml.py
@@ -1,5 +1,28 @@
 import requests
 import xml.etree.ElementTree as et
+import pandas as pd
+import io
+import time
+
+def get_kml_data(kml_root):
+  # index into the kml document structure
+  for document in kml_root.findall('{http://www.opengis.net/kml/2.2}Document'):
+    for placemark in document.findall('{http://www.opengis.net/kml/2.2}Placemark'):
+      for coordinates in placemark.iter('{http://www.opengis.net/kml/2.2}coordinates'):
+        doc_dict = {}
+        doc_dict['coordinates'] = coordinates.text
+        yield doc_dict
+    for folder in document.findall('{http://www.opengis.net/kml/2.2}Folder'):
+      for placemark in folder.findall('{http://www.opengis.net/kml/2.2}Placemark'):
+        for coordinates in placemark.iter('{http://www.opengis.net/kml/2.2}coordinates'):
+          doc_dict = {}
+          doc_dict['coordinates'] = coordinates.text
+          yield doc_dict
+
+        # doc_dict['timespan_begin'] = placemark.find('{http://www.opengis.net/kml/2.2}begin').text
+        # doc_dict['timespan_end']   = placemark.find('{http://www.opengis.net/kml/2.2}end').text
+        # doc_dict['coordinates']    = placemark.findall('{http://www.opengis.net/kml/2.2}coordinates').text
+        # yield doc_dict
 
 parms = {}
 
@@ -15,20 +38,43 @@ parms['maxLon']           = '180'
 url = 'https://sapt.faa.gov/SAPTWS/KmlServlet'
 
 try:
-    r = requests.get(url, params=parms)
-    r.raise_for_status()
-    kmlid = r.json()['kmlid']
+  print(f'Requesting server generation of kml file at url: {url}...')
+  r = requests.get(url, params=parms)
+  r.raise_for_status()
+  kmlid = r.json()['kmlid']
+  print(f'Kml file generated: id is {kmlid}')
 
-    print(kmlid)
+  url = f'https://sapt.faa.gov/SAPTWS/KmlServlet?request_type=getKml&kmlid={kmlid}'
+  headers = {
+    'host' : 'sapt.faa.gov',
+    'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0',
+    'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'accept-language' : 'en-US,en;q=0.5',
+    'accept-encoding' : 'gzip, deflate, br',
+    'connection' : 'keep-alive',
+    'referer' : 'https://sapt.faa.gov/outages.php?outageType=129001450&outageResolution=1.0',
+    'upgrade-insecure-requests' : '1'
+  }
 
-    url = 'https://sapt.faa.gov/SAPTWS/KmlServlet?request_type=getKml&kmlid=' + str(kmlid)
 
-    r = requests.get(url, allow_redirects=True)
-    kml = r.text
+  print(f'Requesting download of kml file at url: {url}...')
+  time.sleep(15) #sleep for a bit to prevent requesting the resource from the server too early and having it not exist
+  r = requests.get(url, headers=headers)
+  r.raise_for_status()
 
-    xtree = et.fromstring(kml)
+  kml_file_name = f'outages-{kmlid}-129001450.kml'
+  f = open(kml_file_name, "w")
+  print(f"Writing retrieved file \"{kml_file_name}\" to disk...")
+  f.write(r.text)
+  f.close
+
+  xml_data = io.StringIO(r.text)
+  etree = et.parse(xml_data)
+  # etree = et.parse("outages-1238-129001450.kml")
+  doc_df = pd.DataFrame(list(get_kml_data(etree.getroot())))
+
+  print(doc_df)
     
-
-except HTTPError:
-    print("The response did not complete successfully")
-    exit
+except requests.HTTPError:
+  print(f'Failed to retrieve file at: {url}...')
+  doc_df = pd.DataFrame(['The request did not complete successfully'], columns=['Error message'])
