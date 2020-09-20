daefe861c1caa0ff1c504293413e35cb8e1a3a8c
bobbykemp <robertjtkemp@gmail.com>
2020-09-20 11:08:37 -0500

    updated gitignore for kmls; finalized simple kml parsing, implemented creation of csv files on qnap for historical analysis in powerbi

--- get-kml.py
+++ get-kml.py
@@ -12,7 +12,7 @@ from dateutil import parser
 def get_kml_data(kml_root):
   # index into the kml document structure
   for document in kml_root.findall('{http://www.opengis.net/kml/2.2}Document'):
-    for placemark in document.findall('{http://www.opengis.net/kml/2.2}Placemark'):
+    for placemark in document.iter('{http://www.opengis.net/kml/2.2}Placemark'):
       for coordinates in placemark.iter('{http://www.opengis.net/kml/2.2}coordinates'):
         doc_dict = {}
         doc_dict['coordinates'] = coordinates.text
@@ -21,48 +21,26 @@ def get_kml_data(kml_root):
         testing_pattern = re.compile(r'Effective on (?P<start_date>\d{4}-\d{2}-\d{2}) from (?P<start_time>\d{2}\:\d{2}\:\d{2}\.\d) until (?P<end_time>\d{2}\:\d{2}\:\d{2}\.\d) (?P<timezone>\w+)')
         outage_text = description.text
         test_text = description.text
+        # GPS Interference test data
         if testing_pattern.search(test_text) is not None:
           testing_match = testing_pattern.search(test_text)
           st = parser.parse(testing_match.group('start_date') + ' ' + testing_match.group('start_time') + ' ' + testing_match.group('timezone'))
           dt = parser.parse(testing_match.group('start_date') + ' ' + testing_match.group('end_time') + ' ' + testing_match.group('timezone'))
           doc_dict['start_time'] = str(st)
           doc_dict['end_time'] = str(dt)
+          doc_dict['data_type'] = 'GPS Interference Testing'
           yield doc_dict
+        # GPS outage data
         if outage_pattern.search(outage_text) is not None:
           outage_match = outage_pattern.search(outage_text)
           st = parser.parse(outage_match.group('start_date') + ' ' + outage_match.group('start_time') + ' ' + outage_match.group('timezone'))
           dt = parser.parse(outage_match.group('end_date') + ' ' + outage_match.group('end_time') + ' ' + outage_match.group('timezone'))
           doc_dict['start_time'] = str(st)
           doc_dict['end_time'] = str(dt)
+          doc_dict['data_type'] = 'GPS Outage'
           yield doc_dict
         doc_dict['description'] = description.text
         yield doc_dict
-    for folder in document.findall('{http://www.opengis.net/kml/2.2}Folder'):
-      for placemark in folder.findall('{http://www.opengis.net/kml/2.2}Placemark'):
-        for coordinates in placemark.iter('{http://www.opengis.net/kml/2.2}coordinates'):
-          doc_dict = {}
-          doc_dict['coordinates'] = coordinates.text
-        for description in placemark.iter('{http://www.opengis.net/kml/2.2}description'):
-          outage_pattern =  re.compile(r'Outage from (?P<start_date>\d{4}-\d{2}-\d{2}) at (?P<start_time>\d\d\:\d\d\:\d\d) to (?P<end_date>\d{4}-\d{2}-\d{2}) at (?P<end_time>\d{2}\:\d{2}\:\d{2}) (?P<timezone>\w+)')
-          testing_pattern = re.compile(r'Effective on (?P<start_date>\d{4}-\d{2}-\d{2}) from (?P<start_time>\d{2}\:\d{2}\:\d{2}\.\d) until (?P<end_time>\d{2}\:\d{2}\:\d{2}\.\d) (?P<timezone>\w+)')
-          outage_text = description.text
-          test_text = description.text
-          if testing_pattern.search(test_text) is not None:
-            testing_match = testing_pattern.search(test_text)
-            st = parser.parse(testing_match.group('start_date') + ' ' + testing_match.group('start_time') + ' ' + testing_match.group('timezone'))
-            dt = parser.parse(testing_match.group('start_date') + ' ' + testing_match.group('end_time') + ' ' + testing_match.group('timezone'))
-            doc_dict['start_time'] = str(st)
-            doc_dict['end_time'] = str(dt)
-            yield doc_dict
-          if outage_pattern.search(outage_text) is not None:
-            outage_match = outage_pattern.search(outage_text)
-            st = parser.parse(outage_match.group('start_date') + ' ' + outage_match.group('start_time') + ' ' + outage_match.group('timezone'))
-            dt = parser.parse(outage_match.group('end_date') + ' ' + outage_match.group('end_time') + ' ' + outage_match.group('timezone'))
-            doc_dict['start_time'] = str(st)
-            doc_dict['end_time'] = str(dt)
-            yield doc_dict
-          doc_dict['description'] = description.text
-          yield doc_dict
 
 headers = {
   'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -138,15 +116,18 @@ except requests.HTTPError:
   print(doc_df)
   exit()
 
-# kml_file_name = f'outages-{kmlid}-129001450.kml'
-# f = open(kml_file_name, "w")
-# print(f"Writing retrieved file \"{kml_file_name}\" to disk...")
-# f.write(r.text)
-# f.close
+kml_file_name = f'outages-{kmlid}-129001450.kml'
+f = open('//10.100.1.248/htix/PowerBI/Outage Reporting/kmls/' + kml_file_name, "w")
+print(f"Writing retrieved file \"{kml_file_name}\" to disk...")
+f.write(r.text)
+f.close()
 
 xml_data = io.StringIO(r.text)
 etree = et.parse(xml_data)
 # etree = et.parse("outages-1238-129001450.kml")
 doc_df = pd.DataFrame(list(get_kml_data(etree.getroot())))
+
+csv_file_name = f'outages-{kmlid}-129001450.csv'
+doc_df.to_csv('//10.100.1.248/htix/PowerBI/Outage Reporting/csvs/' + csv_file_name)
 
 print(doc_df)
